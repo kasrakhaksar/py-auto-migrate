d135883a2cb7883286f86d5243541573cc618c70
kasra khaksar <ksra13khaksar@gmail.com>
2025-08-17 01:01:48 +0330

    fix path bugs

--- py_auto_migrate/migrator.py
+++ py_auto_migrate/migrator.py
@@ -1,3 +1,3 @@
-from .databases_model.mongo_model import MongoToMongo , MongoToMySQL , MongoToPostgres
-from .databases_model.mysql_model import MySQLToMongo , MySQLToMySQL , MySQLToPostgres
-from .databases_model.postgresql_model import PostgresToMongo , PostgresToPostgres , PostgresToMySQL
+from py_auto_migrate.databases_model.mongo_model import MongoToMongo, MongoToMySQL, MongoToPostgres
+from py_auto_migrate.databases_model.mysql_model import MySQLToMongo , MySQLToMySQL , MySQLToPostgres
+from py_auto_migrate.databases_model.postgresql_model import PostgresToMongo , PostgresToPostgres , PostgresToMySQL
