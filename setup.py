764c75483efe5e0455bdee46f6178244db9bf525
kasra khaksar <ksra13khaksar@gmail.com>
2025-10-17 23:54:52 +0330

    merge pypi version with github version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='py-auto-migrate',
-    version='1.1.0',
+    version='0.2.0',
     author='Kasra Khaksar',
     author_email='kasrakhaksar17@gmail.com',
     description='A Tool For Transferring Data, Tables, And Datasets Between Different Databases.',
