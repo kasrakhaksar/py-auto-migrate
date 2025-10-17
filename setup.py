14cd07870729ba59a553d38fa205eef0b6314ad6
kasra khaksar <ksra13khaksar@gmail.com>
2025-10-17 23:24:24 +0330

    fix version name

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='py-auto-migrate',
-    version='0.2',
+    version='2.0',
     author='Kasra Khaksar',
     author_email='kasrakhaksar17@gmail.com',
     description='A Tool For Transferring Data, Tables, And Datasets Between Different Databases.',
